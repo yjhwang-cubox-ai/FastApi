811c6f8d664985a8e9867bc4dafe652b5d1006a0
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-04-17 17:31:08 +0900

    fast api study ing...

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-]
+db = []
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+class City(BaseModel):
+    name:str
+    timezone: str
 
-@app.get("/hello")
+@app.get("/")
 def hello():
     return {"mm": "안녕하세요 파이보"}
+
+@app.get('/cities')
+def get_cities():
+    results = []
+    strs = "http://worldtimeapi.org/api/timezone/Asia/Seoul"
+    for city in db:
+        r = requests.get(strs)
+        cur_time = r.json()['datetime']
+        results.append({})
+    
+    
+    return results
+
+# @app.get('/cities/{city_id}')
+# def get_city(city_id: int):
+
+# @app.get('/cities')
+# def create_city(city_id: int):
+
+# @app.get('/cities/{city_id}')
+# def delete_city(city_id: int):
