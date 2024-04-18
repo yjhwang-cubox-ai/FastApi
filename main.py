3601343c78ce4992d8366ba88c8270f143ca3ffe
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-04-18 09:06:02 +0900

    feat: city

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import requests
 
 app = FastAPI()
 
@@ -16,21 +17,31 @@ def hello():
 
 @app.get('/cities')
 def get_cities():
-    results = []
-    strs = "http://worldtimeapi.org/api/timezone/Asia/Seoul"
+    results = []    
     for city in db:
+        strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
         r = requests.get(strs)
         cur_time = r.json()['datetime']
-        results.append({})
-    
-    
+        results.append({'name':city['name'], 'timezone':city['timezone'], 'current_time':cur_time})
     return results
 
-# @app.get('/cities/{city_id}')
-# def get_city(city_id: int):
+@app.get('/cities/{city_id}')
+def get_city(city_id: int):
+    city = db[city_id-1]
+    strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
+    r = requests.get(strs)
+    
+    cur_time = r.json()['datetime']
+    
+    return {'name':city['name'], 'timezone':city['timezone'], 'current_time':cur_time}
 
-# @app.get('/cities')
-# def create_city(city_id: int):
+@app.post('/cities')
+def create_city(city: City):
+    db.append(city.dict())    
+    return db[-1]
 
-# @app.get('/cities/{city_id}')
-# def delete_city(city_id: int):
+@app.delete('/cities/{city_id}')
+def delete_city(city_id: int):
+    db.pop(city_id-1)
+    
+    return {}
